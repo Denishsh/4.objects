3c36aacf62829e2b5f2dbae5ee27a6f0d227820a
test <test>
2022-04-28 22:03:49 +0300

    fix

--- task.js
+++ task.js
@@ -1,45 +1,40 @@
 function Student(name, gender, age) {
-    this.name = name
-    this.gender = gender
-    this.age = age
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectName
+    this.subject = subjectName;
 }
 
 Student.prototype.addMark = function (mark) {
     if(this.marks === undefined){
-      this.marks = [mark]
+      this.marks = [mark];
       } else {
-        this.marks.push(mark)
+        this.marks.push(mark);
       }
 }
 
-Student.prototype.addMarks = function (mark1,mark2,mark3...) {
-    if(this.marks === undefined){
-      this.marks = []
-      this.marks.push(mark1)
-      this.marks.push(mark2)
-      this.marks.push(mark3)
+Student.prototype.addMarks = function (...marks) {
+    if (this.marks === undefined){
+      this.marks = [...marks];
       } else {
-          this.marks.push(mark1)
-          this.marks.push(mark2)
-          this.marks.push(mark3)
+          this.marks.push(...marks);
       }
 }
 
 Student.prototype.getAverage = function () {
-    let sum = 0
-    for( let mark in marks){
-        sum += mark
+    let sum = 0;
+    for(let mark of this.marks){
+        sum += mark;
     }
 
-    return sum / marks.length
+    return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-    this.excluded = reason
-    delete this.marks
-    delete this.subject
+    this.excluded = reason;
+    delete this.marks;
+    delete this.subject;
 }
